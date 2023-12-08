551004f64bc9973b62b340f1e9d452a7240e5655
Olga Tumanina <olga.tumanina@jedlix.com>
2023-12-08 19:34:19 +0100

    Added AzureAd authentification

---
+++ BlazorPlayground.Web/Middleware/CustomAccountFactory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication.Internal;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Security.Claims;
+using System.Text.Json.Serialization;
+
+namespace BlazorPlayground.Web.Middleware
+{
+    public class CustomAccountFactory : AccountClaimsPrincipalFactory<CustomUserAccount>
+    {
+        public CustomAccountFactory(IAccessTokenProviderAccessor accessor)
+            : base(accessor)
+        {
+
+        }
+
+        public async override ValueTask<ClaimsPrincipal> CreateUserAsync(CustomUserAccount account, RemoteAuthenticationUserOptions options)
+        {
+            var initialUser = await base.CreateUserAsync(account, options);
+
+            //temporary solution, account.Roles should be used 
+            var admins = new List<string>
+            {
+                "John Doe",
+            };
+            if (initialUser.Identity.IsAuthenticated)
+            {
+                var userIdentity = (ClaimsIdentity)initialUser.Identity;
+                var name = userIdentity.Claims.FirstOrDefault(c => c.Type == "name")?.Value;
+                if (admins.Contains(name))
+                {
+                    userIdentity.AddClaim(new Claim(ClaimTypes.Role, "Write"));
+                }
+            }
+
+            return initialUser;
+        }
+    }
+
+    public class CustomUserAccount : RemoteUserAccount
+    {
+        [JsonPropertyName("groups")]
+        public string[] Groups { get; set; }
+
+        [JsonPropertyName("roles")]
+        public string[] Roles { get; set; }
+    }
+}
--- BlazorPlayground.Web/Program.cs
+++ BlazorPlayground.Web/Program.cs
@@ -1,6 +1,8 @@
 using BlazorPlayground.Web;
 using BlazorPlayground.Web.Client;
+using BlazorPlayground.Web.Middleware;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,6 +10,12 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication<RemoteAuthenticationState, CustomUserAccount>(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration.GetSection("ServerApi")["Scopes"]);
+}).AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, CustomUserAccount, CustomAccountFactory>();
 
 var configSection = builder.Configuration.GetSection("Server");
 builder.Services.RegisterClient(configSection.Get<ApiClientConfiguration>());
